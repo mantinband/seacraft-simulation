e427530df1bcb1590eb6ab5b7e8891214b9db6ed
amichai <ami.dan.man@gmail.com>
2018-06-18 03:18:36 +0300

    comments added

--- Port.h
+++ Port.h
@@ -11,12 +11,31 @@
 #include "SeaObject.h"
 #include "Seacraft.h"
 
+/****************************************************/
+/*  Class represents a port.                        */
+/*  the port produces fuel and has an infinite      */
+/*  amount of cargo if a cargo ship wants to load   */
+/*  it supports refueling seacrafts. only one       */
+/*  seacraft can refuel during an update.           */
+/*  if more than one requests, it is added          */
+/*  to refuel queue.                                */
+/****************************************************/
+
 class Port : public SeaObject {
 public:
     Port(string name, Point point, double initialFuel, double hourlyFuelProduction);
+
+    /*  returns string with details regarding
+     *  port's status. location, fuel amount etc'    */
     string getStatusDetails() const override;
+
+    /*  adds given seacraft to refuel queue */
     void addToRefuelQueue(const shared_ptr<Seacraft> &toAdd);
+
+    /*  returns queue length    */
     unsigned long getRefuelQueueLength() const;
+
+    /*  updates ports fuel amount and refuels a ship if needed  */
     void update() override;
 
 private:
